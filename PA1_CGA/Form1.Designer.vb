a3a4d0d287fee13e0d80d3f02582a88e56d694bd
Dwiky Putra <garangman19@gmail.com>
2020-09-07 21:45:59 +0800

    first commit

---
+++ PA1_CGA/Form1.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnDot = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnLine = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblX = New System.Windows.Forms.Label()
+        Me.lblY = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.btnColor = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.tsbtnOpen = New System.Windows.Forms.ToolStripButton()
+        Me.tsbtnSave = New System.Windows.Forms.ToolStripButton()
+        Me.cmbSize = New System.Windows.Forms.ComboBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnDot
+        '
+        Me.btnDot.Location = New System.Drawing.Point(29, 74)
+        Me.btnDot.Name = "btnDot"
+        Me.btnDot.Size = New System.Drawing.Size(40, 23)
+        Me.btnDot.TabIndex = 1
+        Me.btnDot.Text = "Dot"
+        Me.btnDot.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Tools"
+        '
+        'btnLine
+        '
+        Me.btnLine.Location = New System.Drawing.Point(29, 103)
+        Me.btnLine.Name = "btnLine"
+        Me.btnLine.Size = New System.Drawing.Size(40, 23)
+        Me.btnLine.TabIndex = 3
+        Me.btnLine.Text = "Line"
+        Me.btnLine.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.PictureBox1.Location = New System.Drawing.Point(98, 28)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(640, 400)
+        Me.PictureBox1.TabIndex = 13
+        Me.PictureBox1.TabStop = False
+        '
+        'lblX
+        '
+        Me.lblX.AutoSize = True
+        Me.lblX.Location = New System.Drawing.Point(118, 444)
+        Me.lblX.Name = "lblX"
+        Me.lblX.Size = New System.Drawing.Size(13, 13)
+        Me.lblX.TabIndex = 18
+        Me.lblX.Text = "0"
+        '
+        'lblY
+        '
+        Me.lblY.AutoSize = True
+        Me.lblY.Location = New System.Drawing.Point(184, 444)
+        Me.lblY.Name = "lblY"
+        Me.lblY.Size = New System.Drawing.Size(13, 13)
+        Me.lblY.TabIndex = 19
+        Me.lblY.Text = "0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(99, 444)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(20, 13)
+        Me.Label6.TabIndex = 20
+        Me.Label6.Text = "X: "
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(158, 444)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(20, 13)
+        Me.Label7.TabIndex = 21
+        Me.Label7.Text = "Y: "
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(654, 439)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 22
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnColor
+        '
+        Me.btnColor.Location = New System.Drawing.Point(29, 160)
+        Me.btnColor.Name = "btnColor"
+        Me.btnColor.Size = New System.Drawing.Size(40, 23)
+        Me.btnColor.TabIndex = 23
+        Me.btnColor.Text = "Color"
+        Me.btnColor.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsbtnOpen, Me.tsbtnSave})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(825, 25)
+        Me.ToolStrip1.TabIndex = 26
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'tsbtnOpen
+        '
+        Me.tsbtnOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbtnOpen.Image = CType(resources.GetObject("tsbtnOpen.Image"), System.Drawing.Image)
+        Me.tsbtnOpen.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbtnOpen.Name = "tsbtnOpen"
+        Me.tsbtnOpen.Size = New System.Drawing.Size(23, 22)
+        Me.tsbtnOpen.Text = "ToolStripButton1"
+        '
+        'tsbtnSave
+        '
+        Me.tsbtnSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbtnSave.Image = CType(resources.GetObject("tsbtnSave.Image"), System.Drawing.Image)
+        Me.tsbtnSave.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbtnSave.Name = "tsbtnSave"
+        Me.tsbtnSave.Size = New System.Drawing.Size(23, 22)
+        Me.tsbtnSave.Text = "ToolStripButton2"
+        '
+        'cmbSize
+        '
+        Me.cmbSize.DisplayMember = "1"
+        Me.cmbSize.FormattingEnabled = True
+        Me.cmbSize.Items.AddRange(New Object() {"1", "2", "3", "4", "5"})
+        Me.cmbSize.Location = New System.Drawing.Point(62, 133)
+        Me.cmbSize.Name = "cmbSize"
+        Me.cmbSize.Size = New System.Drawing.Size(30, 21)
+        Me.cmbSize.TabIndex = 27
+        Me.cmbSize.Text = "1"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(26, 136)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(30, 13)
+        Me.Label8.TabIndex = 28
+        Me.Label8.Text = "Size:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(825, 479)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.cmbSize)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.btnColor)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblY)
+        Me.Controls.Add(Me.lblX)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnLine)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnDot)
+        Me.Name = "Form1"
+        Me.Text = "Draw Tools"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnDot As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnLine As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblX As Label
+    Friend WithEvents lblY As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents btnColor As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents tsbtnOpen As ToolStripButton
+    Friend WithEvents tsbtnSave As ToolStripButton
+    Friend WithEvents cmbSize As ComboBox
+    Friend WithEvents Label8 As Label
+End Class
---
+++ PA1_CGA/Form1.vb
@@ -0,0 +1,361 @@
+﻿Public Class Form1
+    Dim bmp As New Drawing.Bitmap(640, 400)
+    Dim gfx As Graphics = Graphics.FromImage(bmp)
+    Dim color As Color = Color.Black
+    Dim mode As String = "dot"
+    Dim count As Integer = 0
+    Dim pSize As Integer = 1
+    Dim x1, y1, x2, y2 As Integer
+    Dim lineCase As Integer
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        gfx.FillRectangle(Brushes.White, 0, 0, PictureBox1.Width, PictureBox1.Height)
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub drawLine(X1 As Integer, Y1 As Integer, X2 As Integer, Y2 As Integer)
+        Dim j As Integer = 1
+        Dim dx As Integer
+        Dim dy As Integer
+
+        dx = Math.Abs(X2 - X1)
+        dy = Math.Abs(Y2 - Y1)
+
+        If Y1 = Y2 And X1 <= X2 Then 'case 1
+            For X As Integer = X1 To X2
+                bmp.SetPixel(X, Y1, color)
+            Next
+            lineCase = 1
+        ElseIf Y1 = Y2 And X1 > X2 Then 'case 2
+            For X As Integer = X1 To X2 Step -1
+                bmp.SetPixel(X, Y1, color)
+            Next
+            lineCase = 2
+        ElseIf X1 = X2 And Y1 < Y2 Then 'case 3
+            For Y As Integer = Y1 To Y2
+                bmp.SetPixel(X1, Y, color)
+            Next
+            lineCase = 3
+        ElseIf X1 = X2 And Y1 > Y2 Then 'case 4
+            For Y As Integer = Y1 To Y2 Step -1
+                bmp.SetPixel(X1, Y, color)
+            Next
+            lineCase = 4
+        ElseIf X1 < X2 And Y1 < Y2 And dx = dy Then 'case 5
+            For i As Integer = 0 To dx
+                bmp.SetPixel(X1 + i, Y1 + i, color)
+            Next
+            lineCase = 5
+            If pSize > 1 Then
+                While j < pSize
+                    For i As Integer = 0 To dx - j
+                        bmp.SetPixel(X1 + i + j, Y1 + i, color)
+                        bmp.SetPixel(X1 + i, Y1 + i + j, color)
+                    Next
+                    j = j + 1
+                End While
+            End If
+        ElseIf X1 > X2 And Y1 < Y2 And dx = dy Then 'case 6
+            For i As Integer = 0 To dx
+                bmp.SetPixel(X1 - i, Y1 + i, color)
+            Next
+            lineCase = 6
+            If pSize > 1 Then
+                While j < pSize
+                    For i As Integer = 0 To dx - j
+                        bmp.SetPixel(X1 - i - j, Y1 + i, color)
+                        bmp.SetPixel(X1 - i, Y1 + i + j, color)
+                    Next
+                    j = j + 1
+                End While
+            End If
+        ElseIf X1 > X2 And Y1 > Y2 And dx = dy Then 'case 7
+            For i As Integer = 0 To dx
+                bmp.SetPixel(X1 - i, Y1 - i, color)
+            Next
+            lineCase = 7
+            If pSize > 1 Then
+                While j < pSize
+                    For i As Integer = 0 To dx - j
+                        bmp.SetPixel(X1 - i - j, Y1 - i, color)
+                        bmp.SetPixel(X1 - i, Y1 - i - j, color)
+                    Next
+                    j = j + 1
+                End While
+            End If
+        ElseIf X1 < X2 And Y1 > Y2 And dx = dy Then 'case 8
+            For i As Integer = 0 To dx
+                bmp.SetPixel(X1 + i, Y1 - i, color)
+            Next
+            lineCase = 8
+            If pSize > 1 Then
+                While j < pSize
+                    For i As Integer = 0 To dx - j
+                        bmp.SetPixel(X1 + i + j, Y1 - i, color)
+                        bmp.SetPixel(X1 + i, Y1 - i - j, color)
+                    Next
+                    j = j + 1
+                End While
+            End If
+        Else
+            lineMidPoint(X1, Y1, X2, Y2)
+        End If
+    End Sub
+
+    Private Sub lineMidPoint(X1 As Integer, Y1 As Integer, X2 As Integer, Y2 As Integer)
+        Dim dx, dy, d, dR, dUR, X, Y As Integer
+
+        dx = Math.Abs(X2 - X1)
+        dy = Math.Abs(Y2 - Y1)
+
+        X = X1
+        Y = Y1
+        If X1 < X2 And Y1 < Y2 And Math.Abs(dx) > Math.Abs(dy) Then 'Case 9
+            dR = 2 * dy
+            dUR = 2 * (dy - dx)
+            d = 2 * dy - dx
+            While X <= X2
+                bmp.SetPixel(X, Y, color)
+                X = X + 1
+                If d <= 0 Then
+                    d = d + dR
+                ElseIf d > 0 Then
+                    d = d + dUR
+                    Y = Y + 1
+                End If
+            End While
+            lineCase = 9
+        ElseIf X1 < X2 And Y1 < Y2 And Math.Abs(dx) < Math.Abs(dy) Then 'Case 10
+            dR = -2 * dx
+            dUR = 2 * (dy - dx)
+            d = dy - (2 * dx)
+            While Y <= Y2
+                bmp.SetPixel(X, Y, color)
+                Y = Y + 1
+                If d >= 0 Then
+                    d = d + dR
+                ElseIf d < 0 Then
+                    d = d + dUR
+                    X = X + 1
+                End If
+            End While
+            lineCase = 10
+        ElseIf X1 > X2 And Y1 < Y2 And Math.Abs(dx) < Math.Abs(dy) Then 'Case 11
+            dR = -2 * dx
+            dUR = 2 * (dy - dx)
+            d = dy - (2 * dx)
+            While Y <= Y2
+                bmp.SetPixel(X, Y, color)
+                Y = Y + 1
+                If d >= 0 Then
+                    d = d + dR
+                ElseIf d < 0 Then
+                    d = d + dUR
+                    X = X - 1
+                End If
+            End While
+            lineCase = 11
+        ElseIf X1 > X2 And Y1 < Y2 And Math.Abs(dx) > Math.Abs(dy) Then 'case 12
+            dR = 2 * dy
+            dUR = 2 * (dy - dx)
+            d = 2 * dy - dx
+            While X >= X2
+                bmp.SetPixel(X, Y, color)
+                X = X - 1
+                If d <= 0 Then
+                    d = d + dR
+                ElseIf d > 0 Then
+                    d = d + dUR
+                    Y = Y + 1
+                End If
+            End While
+            lineCase = 12
+        ElseIf X1 > X2 And Y1 > Y2 And Math.Abs(dx) > Math.Abs(dy) Then 'case 13
+            dR = 2 * dy
+            dUR = 2 * (dy - dx)
+            d = 2 * dy - dx
+            While X >= X2
+                bmp.SetPixel(X, Y, color)
+                X = X - 1
+                If d <= 0 Then
+                    d = d + dR
+                ElseIf d > 0 Then
+                    d = d + dUR
+                    Y = Y - 1
+                End If
+            End While
+            lineCase = 13
+        ElseIf X1 > X2 And Y1 > Y2 And Math.Abs(dx) < Math.Abs(dy) Then 'case 14
+            dR = -2 * dx
+            dUR = 2 * (dy - dx)
+            d = dy - (2 * dx)
+            While Y >= Y2
+                bmp.SetPixel(X, Y, color)
+                Y = Y - 1
+                If d >= 0 Then
+                    d = d + dR
+                ElseIf d < 0 Then
+                    d = d + dUR
+                    X = X - 1
+                End If
+            End While
+            lineCase = 14
+        ElseIf X1 < X2 And Y1 > Y2 And Math.Abs(dx) < Math.Abs(dy) Then 'case 15
+            dR = -2 * dx
+            dUR = 2 * (dy - dx)
+            d = dy - (2 * dx)
+            While Y >= Y2
+                bmp.SetPixel(X, Y, color)
+                Y = Y - 1
+                If d >= 0 Then
+                    d = d + dR
+                ElseIf d < 0 Then
+                    d = d + dUR
+                    X = X + 1
+                End If
+            End While
+            lineCase = 15
+        ElseIf X1 < X2 And Y1 > Y2 And Math.Abs(dx) > Math.Abs(dy) Then 'case 16
+            dR = 2 * dy
+            dUR = 2 * (dy - dx)
+            d = 2 * dy - dx
+            While X <= X2
+                bmp.SetPixel(X, Y, color)
+                X = X + 1
+                If d <= 0 Then
+                    d = d + dR
+                ElseIf d > 0 Then
+                    d = d + dUR
+                    Y = Y - 1
+                End If
+            End While
+            lineCase = 16
+        End If
+    End Sub
+
+
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+        If mode = "dot" Then
+            x1 = e.X
+            y1 = e.Y
+            For i As Integer = 0 To pSize - 1 'algorithm to draw dot with specified thickness
+                For j As Integer = 0 To pSize - 1
+                    bmp.SetPixel(x1 + j, y1, color)
+                Next
+                y1 = y1 + 1
+            Next
+            PictureBox1.Image = bmp
+        ElseIf mode = "line" Then
+            bmp.SetPixel(e.X, e.Y, color)
+            PictureBox1.Image = bmp
+            If count = 0 Then
+                x1 = e.X
+                y1 = e.Y
+            ElseIf count = 1 Then
+                x2 = e.X
+                y2 = e.Y
+            End If
+            count = count + 1
+            If count = 2 Then
+                line()
+                count = 0
+            End If
+        End If
+    End Sub
+
+    Private Sub btnDot_Click(sender As Object, e As EventArgs) Handles btnDot.Click
+        mode = "dot"
+    End Sub
+
+    Private Sub btnLine_Click(sender As Object, e As EventArgs) Handles btnLine.Click
+        mode = "line"
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        lblX.Text = e.X
+        lblY.Text = e.Y
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        clear()
+    End Sub
+
+    Private Sub btnColor_Click(sender As Object, e As EventArgs) Handles btnColor.Click
+        If ColorDialog1.ShowDialog = DialogResult.OK Then
+            color = ColorDialog1.Color
+        End If
+    End Sub
+
+    Private Sub clear() 'clear screen
+        count = 0
+        gfx.FillRectangle(Brushes.White, 0, 0, PictureBox1.Width, PictureBox1.Height)
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub line() 'draw line with specified thickness
+        Dim j As Integer = 1
+        drawLine(x1, y1, x2, y2)
+        PictureBox1.Image = bmp
+        If pSize > 1 Then
+            Select Case lineCase
+                Case 1, 2, 9, 12, 13, 16
+                    While j < pSize
+                        If j Mod 2 <> 0 Then
+                            y1 = y1 - j
+                            y2 = y2 - j
+                        Else
+                            y1 = y1 + j
+                            y2 = y2 + j
+                        End If
+                        j = j + 1
+                        drawLine(x1, y1, x2, y2)
+                    End While
+                Case 3, 4, 10, 11, 14, 15
+                    While j < pSize
+                        If j Mod 2 <> 0 Then
+                            x1 = x1 - j
+                            x2 = x2 - j
+                        Else
+                            x1 = x1 + j
+                            x2 = x2 + j
+                        End If
+                        j = j + 1
+                        drawLine(x1, y1, x2, y2)
+                    End While
+                Case 5 To 8
+                    Exit Select
+            End Select
+        End If
+    End Sub
+
+    Private Sub tsbtnOpen_Click(sender As Object, e As EventArgs) Handles tsbtnOpen.Click
+        Dim filename As String
+        OpenFileDialog1.Filter = "PNG Files (*.png)|*.png" +
+                                 "|JPG Files (*.jpg;*.jpeg;*.jpe;*.jfif)|*.jpg;*.jpeg;*.jpe;*.jfif" +
+                                 "|GIF Files (*.gif)|*.gif" +
+                                 "|Bitmap Files (*.bmp;*.dib)|*.bmp;*.dib" +
+                                 "|All Picture Files|*.jpeg;*.jpg;*.jpe;*.jfif;*.png;*.gif;*.bmp;*.dib"
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            filename = OpenFileDialog1.FileName
+            clear()
+            gfx.DrawImage(Image.FromFile(filename), 0, 0)
+            PictureBox1.Image = bmp
+        End If
+    End Sub
+
+    Private Sub tsbtnSave_Click(sender As Object, e As EventArgs) Handles tsbtnSave.Click
+        PictureBox1.DrawToBitmap(bmp, New Rectangle(0, 0, PictureBox1.Width, PictureBox1.Height))
+        SaveFileDialog1.Filter = "PNG Files (*.png)|*.png" +
+                                 "|JPG Files (*.jpg;*.jpeg;*.jpe;*.jfif)|*.jpg;*.jpeg;*.jpe;*.jfif" +
+                                 "|GIF Files (*.gif)|*.gif" +
+                                 "|Bitmap Files (*.bmp;*.dib)|*.bmp;*.dib"
+        If SaveFileDialog1.ShowDialog = DialogResult.OK Then
+            bmp.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Bmp)
+        End If
+    End Sub
+
+    Private Sub cmbSize_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbSize.SelectedIndexChanged
+        pSize = cmbSize.SelectedItem
+    End Sub
+End Class
